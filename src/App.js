33829e78983259c5a3e9c20af803bd8ab5fbc517
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-01 19:24:14 +0100

    Added Letter component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import Letter from "./components/Letter/Letter";
 
 function App() {
-  const gameWord = "string";
-  const [usedLetters, setUsedLetters] = useState([]);
+  // const gameWord = "string";
+  // const [usedLetters, setUsedLetters] = useState([]);
   const abecedary = [
     "A",
     "B",
@@ -32,8 +33,8 @@ function App() {
     "Y",
     "Z",
   ];
-  const [gameStatus, setGameStatus] = useState(null);
-  const [errorCounter, setErrorCounter] = useState(0);
+  // const [gameStatus, setGameStatus] = useState(null);
+  // const [errorCounter, setErrorCounter] = useState(0);
 
   return (
     <div className="App">
@@ -75,87 +76,9 @@ function App() {
       </section>
       <section className="game-result">You're dead!</section>
       <ul className="letters">
-        <li className="letter">
-          <a href="a">A</a>
-        </li>
-        <li className="letter">
-          <a href="b">B</a>
-        </li>
-        <li className="letter">
-          <a href="c">C</a>
-        </li>
-        <li className="letter">
-          <a href="d">D</a>
-        </li>
-        <li className="letter">
-          <a href="e">E</a>
-        </li>
-        <li className="letter">
-          <a href="f">F</a>
-        </li>
-        <li className="letter">
-          <a href="g">G</a>
-        </li>
-        <li className="letter">
-          <a href="h">H</a>
-        </li>
-        <li className="letter">
-          <a href="i">I</a>
-        </li>
-        <li className="letter">
-          <a href="j">J</a>
-        </li>
-        <li className="letter">
-          <a href="k">K</a>
-        </li>
-        <li className="letter">
-          <a href="l">L</a>
-        </li>
-        <li className="letter">
-          <a href="m">M</a>
-        </li>
-        <li className="letter">
-          <a href="n">N</a>
-        </li>
-        <li className="letter">
-          <a href="ñ">Ñ</a>
-        </li>
-        <li className="letter">
-          <a href="o">O</a>
-        </li>
-        <li className="letter">
-          <a href="p">P</a>
-        </li>
-        <li className="letter">
-          <a href="q">Q</a>
-        </li>
-        <li className="letter">
-          <a href="r">R</a>
-        </li>
-        <li className="letter">
-          <a href="s">S</a>
-        </li>
-        <li className="letter">
-          <a href="t">T</a>
-        </li>
-        <li className="letter">
-          <a href="u">U</a>
-        </li>
-        <li className="letter">
-          <a href="v">V</a>
-        </li>
-        <li className="letter">
-          <a href="w">W</a>
-        </li>
-        <li className="letter">
-          <a href="x">X</a>
-        </li>
-        <li className="letter">
-          <a href="y">Y</a>
-        </li>
-        <li className="letter">
-          <a href="z">Z</a>
-        </li>
+        {abecedary.map((letter) => (
+          <Letter letter={letter} />
+        ))}
       </ul>
     </div>
   );
